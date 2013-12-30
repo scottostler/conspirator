1d9a681e43a66cc235af0bd38c613982c15f9733
Lee-kai Wang <free.leekai@gmail.com>
2013-12-31 04:01:52 +0800

    Show final hands in scoresheet
    And change modal title

--- src/scoresheet.js
+++ src/scoresheet.js
@@ -7,9 +7,12 @@ function ScoreSheet(game, $el) {
 
     this.$el.on('hidden', _.bind(this.close, this));
 
-    this.$el.find('.modal-title').text('Game Over');
-
+    this.sortedPlayers = _.sortBy(this.game.players, function(player) {
+        return -player.calculateScore();
+    });
+    
     this.$el.find('.modal-body').empty();
+    this.buildTitleView();
     this.buildSummaryView();
     this.buildPlayerViews();
 
@@ -20,11 +23,58 @@ function ScoreSheet(game, $el) {
 
 ScoreSheet.prototype = new View();
 
-ScoreSheet.prototype.buildSummaryView = function() {
-    var $summary = $('<div>').addClass('summary');
+ScoreSheet.prototype.buildTitleView = function() {
+    if (this.sortedPlayers[0].calculateScore() == this.sortedPlayers[1].calculateScore()) {
+        this.$el.find('.modal-title').text('Tie Game!');
+    } else {
+        this.$el.find('.modal-title').text(this.sortedPlayers[0].name + ' wins!');
+    }
+};
 
-    _.each(this.game.players, _.bind(function(player) {
-        $summary.append($('<div>').text(player.name + ': ' + player.calculateScore() + ' VP'));
+ScoreSheet.prototype.buildSummaryView = function() {
+    var $summaryTable = $('<table>');
+    var $summary = $('<div>').addClass('summary').append($summaryTable);
+    var $tr;
+    _.each(this.sortedPlayers, _.bind(function(player) {
+        $tr = $('<tr>');
+        $tr.append($('<td>').text(player.name + ': '));
+        $tr.append($('<td>').text(player.calculateScore() + 'VP'));
+        
+        player.deckBreakdown = $('<div>');
+        player.sortedDeck = _.sortBy(player.deck.concat(player.discard).concat(player.hand), function(card) {
+            return card.name;
+        });
+        player.sortedDeck = _.sortBy(player.sortedDeck, function(card) {
+            return -card.cost;
+        });
+        player.sortedDeck = _.sortBy(player.sortedDeck, function(card) {
+            if (card.isVictory()) {
+                return 0;
+            } else if (card.isTreasure()) {
+                return 1;
+            } else {
+                return 2;
+            }
+        });
+        var index;
+        var card;
+        var currentCardView;
+        var previousName = '';
+        for (index = 0; index < player.sortedDeck.length; index++) {
+            card = player.sortedDeck[index];
+            if (card.name != previousName) {
+                currentCardView = new CardView(card);
+                player.deckBreakdown.append(currentCardView.$el);
+                badgeCount = 1;
+                currentCardView.setBadgeCount(badgeCount);
+            } else {
+                badgeCount++;
+                currentCardView.setBadgeCount(badgeCount);
+            }
+            previousName = card.name;
+        }
+        $tr.append($('<td>').append(player.deckBreakdown));
+        $summaryTable.append($tr);
     }, this));
 
     $summary.appendTo(this.$el.find('.modal-body'));
